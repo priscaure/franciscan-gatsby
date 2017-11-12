8f7e470abd41dffb30781c7f1f398e73119c0f4e
priscaure <priscaure@gmail.com>
2017-11-12 22:28:24 +0100

    Add AppBar component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,59 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import { withStyles, MuiThemeProvider } from 'material-ui/styles'
+import AppBar from './AppBar'
 
 import './index.css'
-
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
+const classes = this.props.classes;
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+    <MuiThemeProvider theme={theme}>
+        <div>
+            <Helmet
+              title="Gatsby Default Starter"
+              meta={[
+                { name: 'description', content: 'Sample' },
+                { name: 'keywords', content: 'sample, something' },
+              ]}
+            />
+            <AppBar />
+            <div
+              style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '0px 1.0875rem 1.45rem',
+                paddingTop: 0,
+              }}
+            >
+            {children()}
+            </div>
+        </div>
+    </MuiThemeProvider>
 )
 
 TemplateWrapper.propTypes = {
